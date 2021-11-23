157294d7a804d0ec39fc156fd252a0dc14d65e49
Edgar Barrera <ebarrera@americadigital.org>
2021-11-23 10:44:58 -0600

    home add images changes

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -9,31 +9,30 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  
+
   images: any[] = ["./assets/img/softnet/redes.jpg", "./assets/img/softnet/software.jpg"]
   @ViewChild("imageInit") imageInit: ElementRef;
   timer = null
-  
+  indice = 0
 
-  constructor(){}
+  constructor() { }
 
   ngOnInit(): void {
     var that = this
-    var i = 0;
-    this.timer = setInterval(() => {      
-      if(that.images.length == i){
-        i = 0
+    this.timer = setInterval(() => {
+      this.indice++
+      if (that.images.length == this.indice) {
+        this.indice = 0
       }
       //that.imageInit.nativeElement.classList.remove('animate__animated');
-      that.imageInit.nativeElement.classList.remove('animate__slideInUp');
-      that.imageInit.nativeElement.classList   = 'animate__slideInUp';
-      that.imageInit.nativeElement.src = that.images[i];
-      i++   
+     /*  that.imageInit.nativeElement.classList.remove('animate__slideInUp');
+      that.imageInit.nativeElement.classList = 'animate__slideInUp';
+      that.imageInit.nativeElement.src = that.images[i]; */
     }, 3000);
   }
 
   ngOnDestroy(): void {
-    clearInterval(this.timer);  
+    clearInterval(this.timer);
   }
 
 }

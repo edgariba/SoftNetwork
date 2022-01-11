13fc2ae503cf6881d5612714e90d362971f0b20d
Edgar Barrera <ebarrera@americadigital.org>
2022-01-11 11:30:44 -0600

    agregando animaciones

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
 declare var particlesJS: any;
+import * as ScrollMagic from 'ScrollMagic';
+import 'ScrollMagic/scrollmagic/minified/plugins/debug.addIndicators.min.js';
 
 @Component({
   selector: 'app-home',
@@ -32,10 +34,26 @@ export class HomeComponent implements OnInit, OnDestroy {
        that.imageInit.nativeElement.classList = 'animate__slideInUp';
        that.imageInit.nativeElement.src = that.images[i]; */
     }, 3000);
+    this.addAnimations()
   }
 
   ngOnDestroy(): void {
     clearInterval(this.timer);
+  }
+
+
+  addAnimations(){
+    var controller = new ScrollMagic.Controller();
+    var revealElements = document.getElementsByClassName("digit");
+		for (var i=0; i<revealElements.length; i++) { // create a scene for each element
+			new ScrollMagic.Scene({
+								triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
+								offset: 50,												 // start a little later
+								triggerHook: 0.9,
+							})
+							.setClassToggle(revealElements[i], "visible") // add class toggle
+							.addTo(controller);
+    }
   }
 
 }
